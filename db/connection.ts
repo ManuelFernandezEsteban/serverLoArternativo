85408b0174e5c82a71328cf8ce687c9343989911
Manuel1977 <Manuel_fe1977@hotmail.com>
2023-02-25 12:50:42 +0100

    pequeños cambios en el front

--- db/connection.ts
+++ db/connection.ts
@@ -2,10 +2,10 @@ import { Sequelize } from 'sequelize';
 
 
 
-const db = new Sequelize('loalternativo',
-                         'loalternativo',
-                         'pzOc5s[p3kZxdTDx',{
-    host: 'localhost',
+const db = new Sequelize('DB-nativos-tierra',
+                         'nativos-tierra',
+                         'AVNS_UjHl8s8ClOhOFlsZ7Wp',{
+    host: 'nativos-tierra-db-do-user-13555636-0.b.db.ondigitalocean.com',
     dialect:'mysql',
     logging:false,
     port:3306 
