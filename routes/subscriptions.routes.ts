d6165e6f45b43754dd94a0862a8b7c02ce47f128
Manuel1977 <Manuel_fe1977@hotmail.com>
2023-02-28 12:38:22 +0100

    bug subida archivos

---
+++ routes/subscriptions.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { createSession, createSubscription, customerPortal, webHook } from '../controllers/subscriptions.controllers';
+
+
+const router = Router();
+
+router.post('/',createSubscription);
+
+router.post('/webhook',webHook);
+
+router.post('/customer-portal',customerPortal);
+
+router.get('/create-checkout-session',createSession)
+
+
+
+export default router;   
