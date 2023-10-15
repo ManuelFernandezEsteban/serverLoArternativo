aa36d58eeb3a0f8fa0fedaf240917d7fd081d347
Manuel1977 <Manuel_fe1977@hotmail.com>
2023-10-15 18:25:27 +0200

    conecction db

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -6,8 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const db = new sequelize_1.Sequelize('nativos-tierra-db', 'doadmin', 'AVNS_D5uJZSMJZOgMrQih1Q0', {
-    host: 'nativos-tierra-db-do-user-13555636-0.b.db.ondigitalocean.com',
+const db = new sequelize_1.Sequelize('nativos-tierra-db', 'doadmin', 'AVNS_agMqg9qsY1lTK5x9UFV', {
+    host: 'db-nativos-tierra-do-user-13555636-0.b.db.ondigitalocean.com',
     dialect: 'mysql',
     logging: false,
     port: 25060
