913892c611a0ac4b5aef6d5463ef865736a99d25
Manuel1977 <Manuel_fe1977@hotmail.com>
2023-10-15 13:26:37 +0200

    cambio bd

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const db = new sequelize_1.Sequelize('DB-nativos-tierra', 'nativos-tierra', 'AVNS_UjHl8s8ClOhOFlsZ7Wp', {
+const db = new sequelize_1.Sequelize('nativos-tierra-db', 'doadmin', 'AVNS_D5uJZSMJZOgMrQih1Q0', {
     host: 'nativos-tierra-db-do-user-13555636-0.b.db.ondigitalocean.com',
     dialect: 'mysql',
     logging: false,
