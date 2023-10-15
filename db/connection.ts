c01bc2796b403fd9abf8665470c64de455df3bc9
Manuel1977 <Manuel_fe1977@hotmail.com>
2023-10-15 18:18:46 +0200

    conecction db

--- db/connection.ts
+++ db/connection.ts
@@ -3,10 +3,10 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize('db-nativos-tierra',
-                         'nativos-tierra-user',
-                         'AVNS_Y22MnA65e9xC4n64LLL',{
-    host: 'nativos-tierra-db-do-user-13555636-0.b.db.ondigitalocean.com',
+const db = new Sequelize('nativos-tierra-db',
+                         'doadmin',
+                         'AVNS_agMqg9qsY1lTK5x9UFV',{
+    host: 'db-nativos-tierra-do-user-13555636-0.b.db.ondigitalocean.com',
     dialect:'mysql',
     logging:false,
     port:25060
